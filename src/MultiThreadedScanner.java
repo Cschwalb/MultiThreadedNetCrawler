3efd2f0b6a7d30a014839b1fa4a090a27ebef0fa
Caleb Schwalb <calebschwalb@gmail.com>
2024-07-17 20:25:53 -0500

    added comments and edited how errors are handled

--- src/MultiThreadedScanner.java
+++ src/MultiThreadedScanner.java
@@ -14,23 +14,24 @@ public class MultiThreadedScanner {
     }
     public static void ping_scan()
     {
-        String sIP = "192.168.86.";
+        String sIP = "192.168.86."; // this is my subnet, change to yours!
         int startIP = 1;
         int endIP = 254;
-        ExecutorService exSVC = Executors.newFixedThreadPool(THEAD_POOL_SIZE);
+        ExecutorService exSVC = Executors.newFixedThreadPool(THEAD_POOL_SIZE); // max pool size is determined by
+        // your thread size
         for(int i = startIP; i <= endIP; i++)
         {
             final String host = sIP + i;
-            exSVC.submit(()->{
+            exSVC.submit(()->{ // lambda!
                 try
                 {
-                    InetAddress inet = InetAddress.getByName(host);
+                    InetAddress inet = InetAddress.getByName(host); // take the string and
                     if(inet.isReachable(TIMEOUT))
                         System.out.println("IP "+host+" is UP!");
                 }
                 catch(IOException e)
                 {
-                    System.out.println(e.toString());
+                    System.err.println(e.toString());
                 }
             });
         }
