100067b94f37abdc1e8d4af54e80476e10ad448d
Caleb Schwalb <calebschwalb@gmail.com>
2024-07-20 00:08:34 -0500

    fixed a bug on taking in new code and added better error handling

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
             System.out.println("Welcome to the Ping Scanner! :) ");
-            MultiThreadedScanner.ping_scan();
+            System.out.print("Please enter a subnet without the last digit(192.168.86.):  ");
+            Scanner keyboard = new Scanner(System.in);
+            String sIP = keyboard.nextLine();
+            MultiThreadedScanner.ping_scan_range(sIP);
         }
     }
--- src/MultiThreadedScanner.java
+++ src/MultiThreadedScanner.java
@@ -6,18 +6,49 @@ import java.util.concurrent.TimeUnit;
 
 public class MultiThreadedScanner {
     private static final int TIMEOUT = 200;
-    private static final int THEAD_POOL_SIZE = 10;
+    private static final int THREAD_POOL_SIZE = 10;
     public static void main(String []args)
     {
         System.out.println("Starting executor!");
         ping_scan();
+    }
+
+    public static void ping_scan_range(String subnet)
+    {
+        int startIP = 1;
+        int endIP = 254;
+        ExecutorService exSvc = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+        for(int i = startIP; i <= endIP; i++)
+        {
+            final String host = subnet + i;
+            exSvc.submit(()->{
+                try
+                {
+                    InetAddress inet = InetAddress.getByName(host);
+                    if(inet.isReachable(TIMEOUT))
+                        System.out.println("IP "+host+" is UP!");
+                }
+                catch(IOException e)
+                {
+                    System.err.println(e.toString());
+                }
+            });
+        }
+        exSvc.shutdown();
+        try {
+            if (!exSvc.awaitTermination(60, TimeUnit.SECONDS)) {
+                exSvc.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            exSvc.shutdownNow();
+        }
     }
     public static void ping_scan()
     {
         String sIP = "192.168.86."; // this is my subnet, change to yours!
         int startIP = 1;
         int endIP = 254;
-        ExecutorService exSVC = Executors.newFixedThreadPool(THEAD_POOL_SIZE); // max pool size is determined by
+        ExecutorService exSVC = Executors.newFixedThreadPool(THREAD_POOL_SIZE); // max pool size is determined by
         // your thread size
         for(int i = startIP; i <= endIP; i++)
         {
